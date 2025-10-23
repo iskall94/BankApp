54d18a538ac8d827eaec4182a3ff82def3dfe3ae
Isak Erbing <isakerik@gmail.com>
2025-10-23 13:16:43 +0200

    Updated and Removed Ascii file, replaced in MainMenu
    Removed AsciiArt.cs (not needed)
    
    Replaced method from old AsciiArt.cs to MainMenu.cs
    
    Calls the method within the MainMenuStart() method.
    
    Prints a placeholder title to MainMenu.
    
    Test in Program.cs:
    MainMenu mainMenu = new MainMenu;
    mainMenu.MainMenuStart();

--- Menus/MainMenu.cs
+++ Menus/MainMenu.cs
@@ -26,6 +26,7 @@ namespace BankApp.Menus
         {
             while (true)
             {
+                AsciiTitle();
                 string title = "MAIN MENU BANK APP";
 
                 int menuChoice = Run(title);
@@ -46,6 +47,23 @@ namespace BankApp.Menus
                 }
             }
         }
+        public static void AsciiTitle()
+        {
+            string asciiTitle = @"
+PPPP   LL      AAA    CCCC  EEEE  HH  HH   OOO   LL      DDDD   EEEE  RRRR   
+P   P  LL     A   A  CC    EE    HH  HH  O   O  LL      D   D  EE    R   R  
+PPPP   LL     AAAAA  CC    EEEE  HHHHHH  O   O  LL      D   D  EEEE  RRRR   
+P      LL     A   A  CC    EE    HH  HH  O   O  LL      D   D  EE    R R    
+P      LLLLLL A   A   CCCC EEEE  HH  HH   OOO   LLLLLL DDDD   EEEE  R  RR  
 
+NN  NN   AAA   MM  MM EEEE 
+NNN NN  A   A  MMM MM EE   
+NN NNN  AAAAA  MM M M EEEE 
+NN  NN  A   A  MM   M EE   
+NN  NN  A   A  MM   M EEEE
+            ";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(asciiTitle);
+        }
     }
 }

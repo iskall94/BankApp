1362a7ad7e2d75a82ab35b06b4e60b3ce0386173
amandavalencia <amandaavalencia@hotmail.com>
2025-10-27 09:27:06 +0100

    clean up some code

--- Program.cs
+++ Program.cs
@@ -9,43 +9,16 @@ namespace BankApp
     {
         static void Main(string[] args)
         {
-            List<Transaction> accountHistory = new List<Transaction>();
-            List<Transaction> TransactionHistory = new List<Transaction>();
-            List<BankAccount> bankAccounts = new List<BankAccount>();
-
 
             Admin admin = new Admin(Guid.NewGuid(), "admin", "Admin");
 
 
+            //söka om ett lån
 
-            BankAccount paulinasKonto = new BankAccount("paulinas konto", AccountType.Normal, Currency.SEK, 25000);
             BankAccount gabrielsKonto = new BankAccount("gabriels konto", AccountType.Normal, Currency.SEK, 25000);
-            User paulina = admin.CreateUser("lösenordet", "Paulina Porsmyr", paulinasKonto);
+
             User gabriel = admin.CreateUser("lösenord", "Gabriel Kassarp", gabrielsKonto);
 
-            BankAccount resekonto = paulina.CreateBankAccount("Paulinas Resekonto", AccountType.Savings, Currency.SEK, 20000);
-
-
-            AccountNumber fromAcc = paulinasKonto.AccountNumber;
-            AccountNumber toAcc = gabrielsKonto.AccountNumber;
-
-
-
-            decimal value = 10000;
-
-
-            Transaction transx = paulina.CreateTransaction(fromAcc, toAcc, value, "Överförning till Gustav", TransactionType.Normal);
-
-            transx.ExecuteTransaction(transx);
-
-            paulina.ShowAllAccounts();
-
-            paulinasKonto.GetTransactionHistory();
-
-
-
-
-            //söka om ett lån
 
             BankAccount loanFromAdmin = Admin.bankaccount;
             AccountNumber LoanToAcc = gabrielsKonto.AccountNumber;
@@ -53,7 +26,7 @@ namespace BankApp
 
             try
             {
-                Transaction loanTransx = gabriel.CreateLoan(LoanToAcc, loanFromAdmin, 800, gabrielsKonto.Balance, "bil lån", TransactionType.Loan);
+                Transaction loanTransx = gabriel.CreateLoan(LoanToAcc, loanFromAdmin, 80000, gabrielsKonto.Balance, "bil lån", TransactionType.Loan);
                 loanTransx.ExecuteTransaction(loanTransx);
 
             }
